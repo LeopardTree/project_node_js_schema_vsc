daafc0250356833a69f944bf35a6a2a52616daa0
Ludvig Viveland <72435598+LeopardTree@users.noreply.github.com>
2021-12-16 13:23:00 +0100

    Update download.js

--- download.js
+++ download.js
@@ -3,7 +3,7 @@ const superagent = require('superagent').agent()
 const molk_moodle = async() => {
     let dashboard = await superagent
     .post('http://moodle.molk.se/login/index.php')
-    .send({username: 'ludvigvive@gmail.com', password: 'qwer1369'})
+    .send({username: 'ludvigvive@gmail.com', password: ''})
     .set('Content-Type', 'text/html; charset=utf-8')
     console.log(dashboard.text);
 };
